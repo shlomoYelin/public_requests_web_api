d8ca361f02698776491152baef37a41bad9ac913
BatshevaYoffe <73179986+BatshevaYoffe@users.noreply.github.com>
2025-08-27 17:14:02 +0300

    delete unusede controller and actions

--- API/Controllers/RequestController.cs
+++ API/Controllers/RequestController.cs
@@ -2,11 +2,9 @@
 using public_requests.Application.Requests.Models;
 using public_requests.Application.Requests.WorkFlows.Interfaces;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace public_requests.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class RequestController : ControllerBase
     {
@@ -27,12 +25,6 @@ namespace public_requests.API.Controllers
             return Ok(result);
         }
 
-        // GET api/<RequestController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
 
         [HttpPost]
         public IActionResult CreateNewRequest([FromBody] CreateNewRequestModel model)
@@ -40,18 +32,6 @@ namespace public_requests.API.Controllers
             var result = _createNewRequestWorkFlow.Create(model);
 
             return Ok(result);
-        }
-
-        // PUT api/<RequestController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<RequestController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
         }
     }
 }
